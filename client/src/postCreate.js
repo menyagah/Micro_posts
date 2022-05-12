a8337740a1dd10a0f4e5fb9dccfc56fd2d96bd4e
martin <menyagah27@gmail.com>
2022-05-12 23:36:35 +0300

    Use BEM naming convention to name classes and add form logic

--- client/src/postCreate.js
+++ client/src/postCreate.js
@@ -1,15 +1,35 @@
+import axios from 'axios';
 import React from 'react';
+import { useState } from 'react';
 
 function PostCreate() {
+  const [title, setTitle] = useState('');
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await axios.post('http://localhost:4000/posts', {
+      title,
+    });
+    setTitle('');
+  };
+
   return (
     <div>
-      <form>
+      <form className="form" onSubmit={onSubmit}>
         <div className="form__group">
           <label className="form__label">Title</label>
-          <input className="form__input" type="text" />
+          <input
+            className="form__input"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            placeholder="Create Your Post Title Here"
+            type="text"
+            id="message"
+            required
+          />
         </div>
         <div className="form__group">
-          <button className="btn btn--primary">Create Post</button>
+          <button className="btn btn--blue">Create Post</button>
         </div>
       </form>
     </div>
