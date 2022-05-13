2eb5e433f03fe0a8e4225a3f956668aa97da7a3c
martin <menyagah27@gmail.com>
2022-05-13 20:01:34 +0300

    Set up the post event handler

--- event-bus/index.js
+++ event-bus/index.js
@@ -4,3 +4,18 @@ const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
+
+//Set up a post event handler
+app.post('/events', (req, res) => {
+  const event = req.body;
+
+  axios.post('http://localhost:4000/events', event);
+  axios.post('http://localhost:4001/events', event);
+  axios.post('http://localhost:4002/events', event);
+
+  res.send({ status: 'ok' });
+});
+
+app.listen(4005, () => {
+  console.log('Listening on 4005');
+});
