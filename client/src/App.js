8cc47f278e69f649e66e4435230fa07fe1099cec
martin <menyagah27@gmail.com>
2022-05-13 11:02:55 +0300

    Add styling and class name to form element

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
 import PostCreate from './postCreate';
+import PostList from './PostList';
 
 function App() {
   return (
-    <div>
+    <div className="main">
       <h1 className="heading-primary">Create Post</h1>
       <PostCreate />
+      <h1 className="heading-primary">Posts</h1>
+      <PostList />
     </div>
   );
 }
